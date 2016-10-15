a8f2fd1faae8495c026161cad5bfdfc2fea319dd
Alex Nguyen <alex.nguyen@td.com>
2016-10-15 00:11:00 -0400

    Added a bunch of shit, just committing incase something screws up

--- HackWestern/HackWestern/HWNewScheduleVC.swift
+++ HackWestern/HackWestern/HWNewScheduleVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HWNewScheduleVC.swift
 //  HackWestern
 //
 //  Created by Alex Nguyen on 2016-10-14.
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HWNewScheduleVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
-
